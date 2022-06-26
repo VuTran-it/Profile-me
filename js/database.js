2badff397710e40e8a1eb5144e2bcf2a51e9515a
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2022-06-26 20:45:32 +0700

    update switch language

--- js/database.js
+++ js/database.js
@@ -23,7 +23,7 @@ export const homeValue = {
         pText:  " I am front-end developer & photographer. " 
                 +"In addition to the knowledge of website programming, I can also design images, this skill helps a lot in creating a website. "
                 +"Although I am not too good at website programming, I like this job so I quickly acquire knowledge and work hard to learn about website programming.",
-        aText: "Download CV"
+        aText: "<span></span><span></span> Download CV"
     },
 
     vn: {
@@ -31,9 +31,122 @@ export const homeValue = {
         myprofession: "Tôi là một",
         pText: "Tôi là nhà phát triển front-end và nhiếp ảnh gia. Ngoài kiến thức về lập trình website, mình còn có thể thiết kế hình ảnh, kỹ năng này giúp ích rất nhiều cho việc tạo website."
                 +"Tuy mình không quá giỏi về lập trình website nhưng mình rất thích công việc này nên mình tiếp thu nhanh kiến thức và chăm chỉ học về lập trình website.",
-        aText: "Tải Xuống CV"
+        aText: "<span></span><span></span> Tải Xuống CV"
     }
 }
+
+export const aboutValue = {
+    en: {
+        aboutTitle1: "PERSONAL <span>INFOS</span>",
+        aboutText1: "My personal information is here. Any other information you have questions can contact me to ask",
+        infoItemName:  "Name : <span>Trần Anh Vũ</span>",
+        infoItemBirday:  "Birday : <span>3-2-2001</span>",
+        infoItemAge:  "Age : <span> 21 </span>",
+        infoItemEmail:  "Email : <span> vutran.it.2k1@gmail.com </span>",
+        infoItemDegree:  "Degree : <span>University</span>",
+        infoItemPhone:  "Phone : <span>0336591847</span>",
+        infoItemCity:  "City : <span>Ho Chi Minh</span>",
+        infoItemSocialMedia:  "Social Media : <span>Facebook, Zalo</span>",
+        hireMe: "<span></span><span></span> Hire Me",
+        aboutTitle2: "My <span>SKILLS</span>",
+        /*  Education */
+        Education: "Education",
+        EducationTimelineDate1: "2019 - 2021",
+        EducationTimelineTitle1: "Front-end learning route(HTML,CSS,JS) - <span>F8</span>",
+        EducationTimelineItem1: "This course helped me build a website interface"
+                    +"analyze the website interface, make effects with animation"
+                    +".....After completing this course, I knew the basic process of"
+                    +"building a website and was able to build a website myself.",
+        EducationTimelineDate2: "2022 - Present",
+        EducationTimelineTitle2: "Building Websites with ReactJS - <span> F8 </span>",
+        EducationTimelineItem2:   "React is a JavaScript library, dedicated to helping developers"
+                    +"build user interfaces (UIs). In the field of websites and web"
+                    +"applications, UI is a collection of on-screen menus, search"
+                    +"bars, buttons, etc., ie everything a user uses to interact with"
+                    +"a website or app. That's why I chose to learn ReactJS",
+        EducationTimelineDate3: "Coming Soon",
+        EducationTimelineTitle3: "Build website with PHP -",
+        /*  Experience */
+        Experience: "Experience",
+        ExperienceTimelineDate1: "2019 - Present",
+        ExperienceTimelineTitle1: "University Degree - Major In IT - <span>IUH</span>",
+        ExperienceTimelineItem1: " I chose to major in information technology at IUH University to"
+                    +"study. Here, I have a lot of knowledge about technology such as"
+                    +"website programming, network configuration, iot programming,....."
+    },
+
+    vn: {
+        aboutTitle1: "THÔNG <span>TIN</span> <span>CÁ</span> <span>NHÂN</span> ",
+        aboutText1: "Thông tin cá nhân của tôi là ở đây. Mọi thông tin thắc mắc các bạn có thể liên hệ với mình để hỏi",
+        infoItemName:  "Tên : <span>Trần Anh Vũ</span>",
+        infoItemBirday:  "Ngày Sinh : <span> 3-2-2001</span>",
+        infoItemAge:  "Tuổi : <span> 21 </span>",
+        infoItemEmail:  "Email : <span> vutran.it.2k1@gmail.com </span>",
+        infoItemDegree:  "Bằng cấp : <span>Đại Học</span>",
+        infoItemPhone:  "Số điện thoại : <span>0336591847</span>",
+        infoItemCity:  "Thành phố : <span>Ho Chi Minh</span>",
+        infoItemSocialMedia:  "Truyền thông xã hội : <span>Facebook, Zalo</span>",
+        hireMe: "<span></span><span></span> Thuê tôi",
+        aboutTitle2: "KỸ <span>NĂNG</span> <span>CỦA</span> <span>TÔI</span>",
+        /*  Education */
+        Education: "Giáo <span>Dục</span>",
+        EducationTimelineDate1: "2019 - 2021",
+        EducationTimelineTitle1: "Lộ trình học Front-end (HTML, CSS, JS) - <span>F8</span>",
+        EducationTimelineItem1: "Khóa học này đã giúp tôi xây dựng giao diện website, phân tích giao"
+                    +"diện trang web, tạo hiệu ứng chuyển động, ..... Sau khi học xong"
+                    +"khóa học này, tôi đã biết quy trình cơ bản để xây dựng một trang web"
+                    +"và có thể tự xây dựng một trang web.",
+        EducationTimelineDate2: "2022 - Hiện Nay",
+        EducationTimelineTitle2: "Xây dựng trang web với ReactJS - <span> F8 </span>",
+        EducationTimelineItem2:   "React là một thư viện JavaScript, dành riêng để giúp các nhà phát"
+                    +"triển xây dựng giao diện người dùng (UI). Trong lĩnh vực trang web và"
+                    +"ứng dụng web, UI là tập hợp các menu trên màn hình, thanh tìm kiếm,"
+                    +"nút, v.v., tức là mọi thứ mà người dùng sử dụng để tương tác với một"
+                    +"trang web hoặc ứng dụng. Đó là lý do tại sao tôi chọn học ReactJS",
+        EducationTimelineDate3: "Sắp có",
+        EducationTimelineTitle3: "Xây dựng trang web bằng PHP -",
+        /*  Experience */
+        Experience: "Trải <span>Qua</span>",
+        ExperienceTimelineDate1: "2019 - Hiện Nay",
+        ExperienceTimelineTitle1: "Bằng Đại học - Chuyên ngành CNTT - <span>IUH</span>",
+        ExperienceTimelineItem1: " Mình đã chọn chuyên ngành công nghệ thông tin tại trường đại học  "
+                    +"IUH để theo học. Tại đây, mình đã có rất nhiều kiến thức về công nghệ"
+                    +"như lập trình website, cấu hình mạng, lập trình iot,....."
+    },
+}
+
+export const serviceValue = {
+    en: {
+        serviceItemInnerTitle1: "Phone Interface",
+        serviceItemInnerText1: "Compatible with many phones, simple interface easy to operate",
+        serviceItemInnerTitle2: "Code Language",
+        serviceItemInnerText2: "Use a variety of coding languages. Take advantage of the many existing libraries",
+        serviceItemInnerTitle3: "Website Colors",
+        serviceItemInnerText3: "The website will have good contrasting colors that will attract people's attention",
+        serviceItemInnerTitle4: "Code Presentation",
+        serviceItemInnerText4: "The code will be optimized, easy to reuse, easy to maintain in the future",
+        serviceItemInnerTitle5: "Website Images",
+        serviceItemInnerText5: "Website images can be edited to suit and design images for the website",
+        serviceItemInnerTitle6: "Completion Time",
+        serviceItemInnerText6: "The project will be completed on time",
+    },
+
+    vn: {
+        serviceItemInnerTitle1: "Giao diện điện thoại",
+        serviceItemInnerText1: "Tương thích với nhiều loại điện thoại, giao diện đơn giản dễ vận hành",
+        serviceItemInnerTitle2: "Ngôn ngữ code",
+        serviceItemInnerText2: "Sử dụng nhiều ngôn ngữ mã hóa khác nhau. Tận dụng nhiều thư viện hiện có",
+        serviceItemInnerTitle3: "Màu sắc trang web",
+        serviceItemInnerText3: "Trang web sẽ có màu sắc tương phản tốt sẽ thu hút sự chú ý của mọi người",
+        serviceItemInnerTitle4: "Trình bày mã",
+        serviceItemInnerText4: "Code sẽ được tối ưu hóa, dễ sử dụng lại, dễ bảo trì trong tương lai",
+        serviceItemInnerTitle5: "Hình ảnh trang web",
+        serviceItemInnerText5: "Hình ảnh trang web có thể được chỉnh sửa cho phù hợp và thiết kế hình ảnh cho trang web",
+        serviceItemInnerTitle6: "Thời gian hoàn thành",
+        serviceItemInnerText6: "Dự án sẽ hoàn thành đúng thời hạn",
+    }
+}
+
 
 function database() {
 
--- js/switchLanguage.js
+++ js/switchLanguage.js
@@ -1,4 +1,9 @@
-import database, { navValue, homeValue } from "./database.js"
+import database, {
+    navValue,
+    homeValue,
+    aboutValue,
+    serviceValue
+} from "./database.js"
 
 const languageBtn = document.querySelector('.language')
 const defaultLanguage = "EN"
@@ -10,30 +15,11 @@ spanLanguage.innerHTML = "EN";
 languageBtn.addEventListener("click", () => {
     if (spanLanguage.innerHTML === "EN") {
         spanLanguage.innerHTML = "VN";
-        /* nav */
-        listNavSpan[0].innerHTML = navValue.vn.spanHome
-        listNavSpan[1].innerHTML = navValue.vn.spanAbout
-        listNavSpan[2].innerHTML = navValue.vn.spanService
-        listNavSpan[3].innerHTML = navValue.vn.spanPortfolio
-        listNavSpan[4].innerHTML = navValue.vn.spanContact
-        /* home */
-        textHello.innerHTML = homeValue.vn.hello
-        textMyProfession.innerHTML = homeValue.vn.myprofession
-        text_p.innerHTML = homeValue.vn.pText
-        text_a.innerHTML = homeValue.vn.aText
+        handleVietnamese();
+
     } else {
         spanLanguage.innerHTML = "EN";
-        /* nav */
-        listNavSpan[0].innerHTML = navValue.en.spanHome
-        listNavSpan[1].innerHTML = navValue.en.spanAbout
-        listNavSpan[2].innerHTML = navValue.en.spanService
-        listNavSpan[3].innerHTML = navValue.en.spanPortfolio
-        listNavSpan[4].innerHTML = navValue.en.spanContact
-        /* home */
-        textHello.innerHTML = homeValue.en.hello
-        textMyProfession.innerHTML = homeValue.en.myprofession
-        text_p.innerHTML = homeValue.en.pText
-        text_a.innerHTML = homeValue.en.aText
+        handleEnglish();
     }
 
 })
@@ -47,3 +33,137 @@ const textMyProfession = document.querySelector('.home .home-info .my-profession
 const text_p = document.querySelector('.home .home-info p')
 const text_a = document.querySelector('.home .home-info a')
 
+/* About */
+const aboutTitle1 = document.querySelector('.about-content .about-text .aboutTitle1')
+const aboutText1 = document.querySelector('.about-content .about-text .aboutText1')
+const infoItem = document.querySelectorAll('.personal-info .info-item p')
+const hireMe = document.querySelector('.buttons .hire-me')
+const aboutTitle2 = document.querySelector('.about-content .about-text .aboutTitle2')
+/* education */
+const education = document.querySelector('.education .title-glow')
+const EducationTimelineDate = document.querySelectorAll('.education .timeline-item .timeline-date')
+const EducationTimelineTitle = document.querySelectorAll('.education .timeline-item .timeline-title')
+const EducationTimelineItem = document.querySelectorAll('.education .timeline-item .timeline-text')
+
+/* experience */
+const experience = document.querySelector('.experience .title-glow')
+const ExperienceTimelineDate = document.querySelectorAll('.experience .timeline-item .timeline-date')
+const ExperienceTimelineTitle = document.querySelectorAll('.experience .timeline-item .timeline-title')
+const ExperienceTimelineItem = document.querySelectorAll('.experience .timeline-item .timeline-text')
+
+
+/* service */
+const serviceItemInnerH4 = document.querySelectorAll('.service .service-item h4')
+const serviceItemInnerP = document.querySelectorAll('.service .service-item p')
+
+
+function handleVietnamese() {
+    /* nav */
+    listNavSpan[0].innerHTML = navValue.vn.spanHome
+    listNavSpan[1].innerHTML = navValue.vn.spanAbout
+    listNavSpan[2].innerHTML = navValue.vn.spanService
+    listNavSpan[3].innerHTML = navValue.vn.spanPortfolio
+    listNavSpan[4].innerHTML = navValue.vn.spanContact
+    /* home */
+    textHello.innerHTML = homeValue.vn.hello
+    textMyProfession.innerHTML = homeValue.vn.myprofession
+    text_p.innerHTML = homeValue.vn.pText
+    text_a.innerHTML = homeValue.vn.aText
+    /* about */
+    aboutTitle1.innerHTML = aboutValue.vn.aboutTitle1
+    aboutText1.innerHTML = aboutValue.vn.aboutText1
+    infoItem[0].innerHTML = aboutValue.vn.infoItemName
+    infoItem[1].innerHTML = aboutValue.vn.infoItemBirday
+    infoItem[2].innerHTML = aboutValue.vn.infoItemAge
+    infoItem[3].innerHTML = aboutValue.vn.infoItemEmail
+    infoItem[4].innerHTML = aboutValue.vn.infoItemDegree
+    infoItem[5].innerHTML = aboutValue.vn.infoItemPhone
+    infoItem[6].innerHTML = aboutValue.vn.infoItemCity
+    infoItem[7].innerHTML = aboutValue.vn.infoItemSocialMedia
+    hireMe.innerHTML = aboutValue.vn.hireMe
+    aboutTitle2.innerHTML = aboutValue.vn.aboutTitle2
+    /* education */
+    education.innerHTML = aboutValue.vn.Education
+    EducationTimelineDate[0].innerHTML = aboutValue.vn.EducationTimelineDate1
+    EducationTimelineTitle[0].innerHTML = aboutValue.vn.EducationTimelineTitle1
+    EducationTimelineItem[0].innerHTML = aboutValue.vn.EducationTimelineItem1
+    EducationTimelineDate[1].innerHTML = aboutValue.vn.EducationTimelineDate2
+    EducationTimelineTitle[1].innerHTML = aboutValue.vn.EducationTimelineTitle2
+    EducationTimelineItem[1].innerHTML = aboutValue.vn.EducationTimelineItem2
+    EducationTimelineDate[2].innerHTML = aboutValue.vn.EducationTimelineDate3
+    EducationTimelineTitle[2].innerHTML = aboutValue.vn.EducationTimelineTitle3
+    /* experience */
+    experience.innerHTML = aboutValue.vn.Experience
+    ExperienceTimelineDate[0].innerHTML = aboutValue.vn.ExperienceTimelineDate1
+    ExperienceTimelineTitle[0].innerHTML = aboutValue.vn.ExperienceTimelineTitle1
+    ExperienceTimelineItem[0].innerHTML = aboutValue.vn.ExperienceTimelineItem1
+    /* service */
+    serviceItemInnerH4[0].innerHTML = serviceValue.vn.serviceItemInnerTitle1
+    serviceItemInnerP[0].innerHTML = serviceValue.vn.serviceItemInnerText1
+    serviceItemInnerH4[1].innerHTML = serviceValue.vn.serviceItemInnerTitle2
+    serviceItemInnerP[1].innerHTML = serviceValue.vn.serviceItemInnerText2
+    serviceItemInnerH4[2].innerHTML = serviceValue.vn.serviceItemInnerTitle3
+    serviceItemInnerP[2].innerHTML = serviceValue.vn.serviceItemInnerText3
+    serviceItemInnerH4[3].innerHTML = serviceValue.vn.serviceItemInnerTitle4
+    serviceItemInnerP[3].innerHTML = serviceValue.vn.serviceItemInnerText4
+    serviceItemInnerH4[4].innerHTML = serviceValue.vn.serviceItemInnerTitle5
+    serviceItemInnerP[4].innerHTML = serviceValue.vn.serviceItemInnerText5
+    serviceItemInnerH4[5].innerHTML = serviceValue.vn.serviceItemInnerTitle6
+    serviceItemInnerP[5].innerHTML = serviceValue.vn.serviceItemInnerText6
+
+}
+
+function handleEnglish() {
+    /* nav */
+    listNavSpan[0].innerHTML = navValue.en.spanHome
+    listNavSpan[1].innerHTML = navValue.en.spanAbout
+    listNavSpan[2].innerHTML = navValue.en.spanService
+    listNavSpan[3].innerHTML = navValue.en.spanPortfolio
+    listNavSpan[4].innerHTML = navValue.en.spanContact
+    /* home */
+    textHello.innerHTML = homeValue.en.hello
+    textMyProfession.innerHTML = homeValue.en.myprofession
+    text_p.innerHTML = homeValue.en.pText
+    text_a.innerHTML = homeValue.en.aText
+    /* about */
+    aboutTitle1.innerHTML = aboutValue.en.aboutTitle1
+    aboutText1.innerHTML = aboutValue.en.aboutText1
+    infoItem[0].innerHTML = aboutValue.en.infoItemName
+    infoItem[1].innerHTML = aboutValue.en.infoItemBirday
+    infoItem[2].innerHTML = aboutValue.en.infoItemAge
+    infoItem[3].innerHTML = aboutValue.en.infoItemEmail
+    infoItem[4].innerHTML = aboutValue.en.infoItemDegree
+    infoItem[5].innerHTML = aboutValue.en.infoItemPhone
+    infoItem[6].innerHTML = aboutValue.en.infoItemCity
+    infoItem[7].innerHTML = aboutValue.en.infoItemSocialMedia
+    hireMe.innerHTML = aboutValue.en.hireMe
+    aboutTitle2.innerHTML = aboutValue.en.aboutTitle2
+    /* education */
+    education.innerHTML = aboutValue.en.Education
+    EducationTimelineDate[0].innerHTML = aboutValue.en.EducationTimelineDate1
+    EducationTimelineTitle[0].innerHTML = aboutValue.en.EducationTimelineTitle1
+    EducationTimelineItem[0].innerHTML = aboutValue.en.EducationTimelineItem1
+    EducationTimelineDate[1].innerHTML = aboutValue.en.EducationTimelineDate2
+    EducationTimelineTitle[1].innerHTML = aboutValue.en.EducationTimelineTitle2
+    EducationTimelineItem[1].innerHTML = aboutValue.en.EducationTimelineItem2
+    EducationTimelineDate[2].innerHTML = aboutValue.en.EducationTimelineDate3
+    EducationTimelineTitle[2].innerHTML = aboutValue.en.EducationTimelineTitle3
+    /* experience */
+    experience.innerHTML = aboutValue.en.Experience
+    ExperienceTimelineDate[0].innerHTML = aboutValue.en.ExperienceTimelineDate1
+    ExperienceTimelineTitle[0].innerHTML = aboutValue.en.ExperienceTimelineTitle1
+    ExperienceTimelineItem[0].innerHTML = aboutValue.en.ExperienceTimelineItem1
+    /* service */
+    serviceItemInnerH4[0].innerHTML = serviceValue.en.serviceItemInnerTitle1
+    serviceItemInnerP[0].innerHTML = serviceValue.en.serviceItemInnerText1
+    serviceItemInnerH4[1].innerHTML = serviceValue.en.serviceItemInnerTitle2
+    serviceItemInnerP[1].innerHTML = serviceValue.en.serviceItemInnerText2
+    serviceItemInnerH4[2].innerHTML = serviceValue.en.serviceItemInnerTitle3
+    serviceItemInnerP[2].innerHTML = serviceValue.en.serviceItemInnerText3
+    serviceItemInnerH4[3].innerHTML = serviceValue.en.serviceItemInnerTitle4
+    serviceItemInnerP[3].innerHTML = serviceValue.en.serviceItemInnerText4
+    serviceItemInnerH4[4].innerHTML = serviceValue.en.serviceItemInnerTitle5
+    serviceItemInnerP[4].innerHTML = serviceValue.en.serviceItemInnerText5
+    serviceItemInnerH4[5].innerHTML = serviceValue.en.serviceItemInnerTitle6
+    serviceItemInnerP[5].innerHTML = serviceValue.en.serviceItemInnerText6
+}
