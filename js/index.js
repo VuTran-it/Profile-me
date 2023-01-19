3c42e2fa503877be46ebb3d58f7797be2df42804
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2023-01-19 09:01:21 +0700

    fix bug

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* Typing animation */
 var typed = new Typed(".typing", {
-    strings: ["", "Font-end Developer", "Photographer"],
+    strings: ["", "Front-end Developer", "Photographer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
